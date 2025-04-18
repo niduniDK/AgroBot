823b8e2c13cab6eeb029250d5a38b788bb17c136
niduniDK <nidunikasige@gmail.com>
2025-04-19 00:37:28 +0530

    Add backend logic for get disease details

--- Backend/routers/get_diseases.py
+++ Backend/routers/get_diseases.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 from dotenv import load_dotenv
 import requests
 import os
@@ -15,7 +15,12 @@ API_URL = f"https://perenual.com/api/pest-disease-list?key={API_KEY}"
 class Disease(BaseModel):
     id: int
     common_name: str
+    scientific_name: str
+    other_names: List[str]
+    family: str
     image_url: str
+    descriptions: List[Dict[str, str]]
+    solutions: List[Dict[str, str]]
 
     class Config:
         orm_mode = True
@@ -30,7 +35,12 @@ def get_diseases_list():
         diseases = [Disease(
             id=disease['id'],
             common_name=disease['common_name'],
-            image_url=disease['images'][0]['regular_url'] if disease.get('images') else ""
+            scientific_name=disease['scientific_name'],
+            other_names=[name for name in disease['other_name']] if disease.get('other_name') else [],
+            family=disease['family'] if disease.get('family') else "",
+            image_url=disease['images'][0]['regular_url'] if disease.get('images') else "",
+            descriptions=[{"subtitle": item["subtitle"], "description":item["description"]} for item in disease['description']],
+            solutions=[{"subtitle": item["subtitle"], "description":item["description"]} for item in disease['solution']]
         ) for disease in data['data']]
         return diseases
     except requests.RequestException as e:
