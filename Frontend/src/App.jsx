3c0e5dbd9fff636a62447db08bbca236b5b2e5f2
niduniDK <nidunikasige@gmail.com>
2025-04-19 00:39:15 +0530

    Add route for detail page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import Details from './pages/details'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/home' element={<Home/>}></Route>
+          <Route path='/disease_details' element={<Details/>}></Route>
         </Routes>
       </Router>
     </>
