8676257babc8c22c007d5cfe9ef95ac9e557320a
niduniDK <nidunikasige@gmail.com>
2025-04-19 16:17:24 +0530

    Page linked to home page

---
+++ Frontend/src/pages/virtualassistant.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VirtualAssistant() {
+    return (
+        <div>
+            Hi! I am your virtual assistant. How can I help you today?
+        </div>
+    )
+}
+
+export default VirtualAssistant;
