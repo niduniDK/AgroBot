eda3fda389ffe91668b1d703a220b8e55403f7f2
niduniDK <nidunikasige@gmail.com>
2025-07-08 17:38:40 +0530

    Update

--- Backend/llm/gemini_client.py
+++ Backend/llm/gemini_client.py
@@ -13,6 +13,7 @@ def generate_response(query: str, history: list) -> str:
         f"You are AgroBot, an AI assistant for agriculture. "
         f"Answer the user questions in a concise and informative manner. "
         f"Use simple words that can be understood by a person with poor literacy.Give the response line by line. Use the {history} to understand messages.\n"
+        f"Arrange the response in a way that it is human readable in a chat application.Give your response in point form. Give details with respesct to different locations if asked.And give response in queried language. Add emojis accordingly\n"
         f"Question: {query}\nResponse:"
     )
     try:
